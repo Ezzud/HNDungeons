762d484c052ffc77e0be3803b24cabf6279e936e
i4dk01 <125922925+i4dk01@users.noreply.github.com>
2024-10-20 14:24:10 -0700

    Initial

---
+++ src/main/java/gg/horizonnetwork/HNDungeons/HNDungeons.java
@@ -0,0 +1,17 @@
+package gg.horizonnetwork.HNDungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HNDungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
