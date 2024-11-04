1d3a3c10eca45eb943bb588a16cb8a6b6c3ea265
Ezzud <contact@ezzud.fr>
2024-11-04 15:42:11 +0100

    Update armor stand spawning to protocollib clientside entity

--- src/main/java/gg/horizonnetwork/HNDungeons/listener/DamageListener.java
+++ src/main/java/gg/horizonnetwork/HNDungeons/listener/DamageListener.java
@@ -1,5 +1,9 @@
 package gg.horizonnetwork.HNDungeons.listener;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
 import gg.horizonnetwork.HNDungeons.HNDungeons;
 import gg.horizonnetwork.HNDungeons.utils.RandomUtil;
 import gg.techtide.tidelib.logger.TideLogger;
@@ -15,7 +19,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import java.lang.reflect.InvocationTargetException;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 import static java.util.regex.Pattern.compile;
@@ -55,15 +64,50 @@ public class DamageListener extends SimpleTideListener<HNDungeons> {
                     Entity victim = event.getEntity();
                     Location spawnLocation = RandomUtil.getRandomLocationAround(victim, 0.3);
                     String damages = formatDbl(event.getDamage());
+                    ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+
+                    PacketContainer packet = protocolManager.createPacket(PacketType.Play.Server.SPAWN_ENTITY);
+
+                    // Entity ID
+                    int entityID = new Random().nextInt(9999);
+                    packet.getIntegers().write(0, entityID);
+                    // Entity Type
+                    packet.getEntityTypeModifier().write(0, EntityType.ARMOR_STAND);
+                    packet.getIntegers().write(7, 0);
+                    packet.getUUIDs().write(0, UUID.randomUUID());
+                    // Set location
+                    packet.getDoubles().write(1, spawnLocation.getY());
+                    packet.getDoubles().write(0, spawnLocation.getX());
+                    packet.getDoubles().write(2, spawnLocation.getZ());
+
+                    try {
+                        protocolManager.sendServerPacket((Player)entityEvent.getDamager(), packet);
+                        ((Player)entityEvent.getDamager()).sendMessage("show78");
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+
+                    /*
                     ArmorStand armorStand = (ArmorStand) victim.getWorld().spawnEntity(spawnLocation, EntityType.ARMOR_STAND);
                     armorStand.setGravity(false);
                     armorStand.setInvisible(true);
                     armorStand.setCustomName(ChatColor.RED + damages + " ❤");
                     armorStand.setCustomNameVisible(true);
                     armorStand.setInvulnerable(true);
-
+                    */
                     TideLogger.console("Vector " + victim.getVelocity().getX() + " " + victim.getVelocity().getY() + " " + victim.getVelocity().getZ());
-                    Bukkit.getServer().getScheduler().runTaskLater(plugin, armorStand::remove, 20L);
+                    Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {
+                        PacketContainer delpacket = protocolManager.createPacket(PacketType.Play.Server.ENTITY_DESTROY);
+                        List<Integer> entityIDs = new ArrayList<>();
+                        entityIDs.add(entityID);
+                        delpacket.getIntLists().write(0, entityIDs);
+
+                        try {
+                            protocolManager.sendServerPacket((Player) entityEvent.getDamager(), delpacket);
+                        } catch (InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    }, 20L);
                 }
             }
         }
